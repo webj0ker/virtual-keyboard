2f7da1b079ed85734dab5d689fa503912b2f7f71
Stas <stanislav.shendryk@gmail.com>
2022-05-10 01:38:03 +0300

    refactor: eslint

--- virtual-keyboard/.eslintrc.js
+++ virtual-keyboard/.eslintrc.js
@@ -1,22 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    'parserOptions': {
+        'ecmaVersion': 'latest'
     },
-    "rules": {
-        "semi": "off",
-        "no-plusplus": "off",
-        "quotes": ["error", "single"],
-      'class-methods-use-this': 'off',
-      'no-return-assign': 'off',
-      'no-param-reassign': 'off',
-      'default-case': 'off',
-      'no-unused-expressions': 'off',
-      'no-restricted-syntax': 'off',
-      'linebreak-style': 'off',
+    'rules': {
+        'semi': 'off',
+        'no-plusplus': 'off',
+        'quotes': ['error', 'single'],
+        'class-methods-use-this': 'off',
+        'no-return-assign': 'off',
+        'no-param-reassign': 'off',
+        'default-case': 'off',
+        'no-unused-expressions': 'off',
+        'no-restricted-syntax': 'off',
+        'linebreak-style': 'off',
     },
-    }
 }
